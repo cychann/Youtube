f6383387d2ce88fdb383bdf94869514bf66f9c15
cychann <skcbdlhc129@naver.com>
2021-12-21 21:22:49 +0900

    리스트 아이템 만들기

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = (props) => (
+  <li className={styles.video}>
+    <img
+      className={styles.thumbnail}
+      src={props.video.snippet.thumbnails.medium.url}
+      alt=""
+    />
+    <div className={styles.video_description}>
+      <span className={styles.title}>{props.video.snippet.title}</span>
+      <span className={styles.channel}>{props.video.snippet.channelTitle}</span>
+    </div>
+  </li>
+);
 
 export default VideoItem;
